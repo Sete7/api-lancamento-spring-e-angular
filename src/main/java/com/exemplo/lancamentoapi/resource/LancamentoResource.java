747ae4cba28691f0505724a22c431ef7c9fb0330
Junio Santos <junio.santos.ps@gmail.com>
2022-09-01 07:50:22 -0300

    Remover lancamento method controle

--- src/main/java/com/exemplo/lancamentoapi/resource/LancamentoResource.java
+++ src/main/java/com/exemplo/lancamentoapi/resource/LancamentoResource.java
@@ -16,12 +16,14 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.lancamentoapi.event.RecursoCriadoEvent;
@@ -73,6 +75,12 @@ public class LancamentoResource {
 		String mensagemDesenvolvedor = ex.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return ResponseEntity.badRequest().body(erros);
+	}
+	
+	@DeleteMapping(value="{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removerLancamento(@PathVariable Long codigo) {
+		repository.deleteById(codigo);
 	}
 
 }
